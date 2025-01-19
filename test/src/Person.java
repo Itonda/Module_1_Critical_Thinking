60f4b10bddeaa8f5854fa27f48b2379fcf84f78c
Connor <anthellic@gmail.com>
2025-01-19 14:26:10 -0700

    Added user input for attributes and printing of object

--- test/src/Person.java
+++ test/src/Person.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Person {
     String firstName;
     String lastName;
@@ -5,13 +7,44 @@ public class Person {
     String city;
     String zipCode;
 
-    public Person(String firstName, String lastName, String streetAddress, String city, String zipCode) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.streetAddress = streetAddress;
-        this.city = city;
-        this.zipCode = zipCode;
+    public Person() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter First Name: ");
+        this.firstName = scanner.nextLine();
+
+        System.out.print("Enter Last Name: ");
+        this.lastName = scanner.nextLine();
+
+        System.out.print("Enter Street Address: ");
+        this.streetAddress = scanner.nextLine();
+
+        System.out.print("Enter City: ");
+        this.city = scanner.nextLine();
+
+        System.out.print("Enter Zip Code: ");
+        this.zipCode = scanner.nextLine();
     }
+
+    public static void main(String[] args) {
+        Person person = new Person();
+        System.out.println(person);
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Person: {" +
+                "First Name: " + firstName + " / " +
+                "Last Name: " + lastName + " / " +
+                "Address: " + streetAddress + " / " +
+                "City: " + city + "}";
+    }
+
+
+
+
 
 
 }
