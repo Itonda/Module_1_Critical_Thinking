1cfe837b57ef847b4ada75b9dbf06d3c1c7e2326
Connor <anthellic@gmail.com>
2025-01-19 13:26:52 -0700

    Added class and constructor

---
+++ test/src/Person.java
@@ -0,0 +1,17 @@
+public class Person {
+    String firstName;
+    String lastName;
+    String streetAddress;
+    String city;
+    String zipCode;
+
+    public Person(String firstName, String lastName, String streetAddress, String city, String zipCode) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.streetAddress = streetAddress;
+        this.city = city;
+        this.zipCode = zipCode;
+    }
+
+
+}
